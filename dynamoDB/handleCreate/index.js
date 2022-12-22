8723488518e1a614c02c3b218e6ad21abfab1433
Camilla Rees <camilla.c.rees@gmail.com>
2022-12-21 18:10:17 -0700

    updated handlers code

--- dynamoDB/handleCreate/index.js
+++ dynamoDB/handleCreate/index.js
@@ -1,9 +1,27 @@
+const dynamoose = require('dynamoose');
+
+const schema = new dynamoose.Schema({
+    "id": String,
+    "name": String,
+    "phone": String,
+})
+
+const peopleModel = dynamoose.model('people-table', schema);
 
 exports.handler = async(event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
-    };
+   
+      let parsedBody = JSON.parse(event.body);
+      console.log('parsed.body', parsedBody)
+   
+    const response = { statusCode: null, body:null };
+    try {
+        let result = await peopleModel.create(parsedBody);
+        response.statusCode = 200;
+        response.body = JSON.stringify(result);
+        
+    } catch(e){
+        response.statusCode = 500;
+        response.body = JSON.stringify(e.message)
+    }
     return response;
   };
